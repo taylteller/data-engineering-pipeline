978e283fab86e4ceba56ab6ab16ffc41ea399276
Taylor Najjar <16545112+taylteller@users.noreply.github.com>
2022-02-03 21:48:21 -0500

    data quality checks

--- helpers.py
+++ helpers.py
@@ -4,7 +4,11 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, isnan, when, count, col
 from pyspark.sql.types import StringType
 
+
 def show_total_missing_values(spark_df):
+    """
+    Display the number and percentage of null or invalid values per dataframe field
+    """
     row_count = spark_df.count()
     
     # tally the number of nulls or NaNs in a column
@@ -21,6 +25,39 @@ def show_total_missing_values(spark_df):
     
 @udf(StringType())    
 def convert_sas_date_to_datetime(sas):
+    """
+    Accepts a SAS date and returns it as a datetime
+    """
     if sas:
         return (datetime(1960, 1, 1).date() + timedelta(sas)).isoformat()
     return None
+
+
+def tables_exist(df_list):
+    """
+    Take a list of dataframes and ensure they exist
+    """
+    all_exist = True
+    for i, df in enumerate(df_list):
+        if df is None:
+            all_exist = False
+            print(f'Dataframe at index {i} does not exist')
+    if all_exist == True:
+        print('All tables exist')
+        
+
+def tables_contain_data(df_list):
+    """
+    Take a list of dataframes and ensure they all contain content.
+    Also print the number of rows for each dataframe
+    """
+    all_contain = True
+    for i, df in enumerate(df_list):
+        count = df.count()
+        if count == 0:
+            all_contain = False
+            print(f'Dataframe at index {i} does not contain any rows')
+        else:
+            print(f'Dataframe at index {i} contains {count} rows')
+    if all_contain == True:
+        print('All tables contain content')
